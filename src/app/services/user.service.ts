50fe3f8aef63a8f5794e83b4c1c09bdb7e5bcb96
Carlos Menchu <carlosmenchu2@gmail.com>
2021-04-24 23:41:52 -0600

    Creacion del servicio de registro se usuarios

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { User } from '../interfaces/user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private userCollection: AngularFirestoreCollection<User>;
+
+
+
+
+  constructor(private _afs: AngularFirestore ) {
+    this.userCollection = this._afs.collection<User>('users');
+  }
+
+  createUser(userData:User, idUser?:string) {
+
+    return new Promise(async(resolve, reject)=>{
+
+      try {
+        const id = idUser ||  this._afs.createId();
+        const data = {id,...userData};
+        const result = this.userCollection.doc(id).set(data);
+
+        resolve(result);
+
+      } catch (error) {
+        reject(error.message);
+      }
+    });
+  }
 }
