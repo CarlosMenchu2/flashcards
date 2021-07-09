1f18f30dcab1b72f73cf52a3056ae7f3c1f6e5db
Carlos Menchu <carlosmenchu2@gmail.com>
2021-07-08 21:44:06 -0600

    Insercion de una nueva pregunta en firebase

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
-
+import { Question } from "../interfaces/question";
 
 
 @Injectable({
@@ -9,11 +10,30 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/fire
 })
 export class QuestionService {
 
+  private questionCollection: AngularFirestoreCollection<Question>;
 
-
-  constructor(private afs: AngularFirestore ) {
-
+  constructor(private _afs: AngularFirestore ) {
+    this.questionCollection = this._afs.collection<Question>('questions');
   }
 
+  createQuestion(question:Question) {
 
+    return new Promise(async(resolve,reject)=>{
+
+      try {
+        const id = this._afs.createId();
+        const data = {id, ...question};
+        const result = await this.questionCollection.doc(id).set(data);
+        resolve(result);
+      } catch (error) {
+        reject(error.message);
+      }
+    });
+  }
+
+  getQuestions(idCategory:string) {
+    return this._afs.collection<Question>('questions',ref=> ref.where('idCategory','==',idCategory)).snapshotChanges().pipe(
+      map(actions => actions.map(a => a.payload.doc.data() as Question))
+    );
+  }
 }
